7edd54b24c902d626884d0018019daeac99ddeb8
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-03 15:22:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,5 +1,6 @@
 class script(object):
-    START_TEXT = """𝙷𝙴𝙻𝙾 {user_mention},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 Rename Bot, I am file renamer bot, Please sent any telegram Document 📄 Or Video 📹 and enter new filenameto rename it"""
+    START_TEXT = """ <b> 𝙷𝙴𝙻𝙾 {user_mention} ,\n 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 Rename Bot, I am file renamer bot, Please sent any telegram Document 📄 Or Video 📹 and enter new filenameto rename it <\b>  \n.
+<b>Bot Maintained By: @TamilBots </b> \n
+"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
