0479291d82924acfcf62d613fca23e2095652f84
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-03 09:37:53 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,5 @@
+class script(object):
+    START_TXT = """𝙷𝙴𝙻𝙾 {},
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, I am file renamer bot, Please sent any telegram Document 📄 Or Video 📹 and enter new filenameto rename it"""
+    HELP_TXT = """𝙷𝙴𝚈 {}
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
