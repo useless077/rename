fed5421f804584b5d2a05c3786667b45dba5975b
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-04 00:10:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,37 +4,84 @@ import humanize
 from helper.database import  insert 
 from Script import script
 
-@Client.on_message(filters.command(["start"]))
-async def start(client, message):
-          await client.send_message(
-          chat_id=message.chat.id,
-          text=script.START_TEXT.format(user_mention=message.from_user.mention),
-          parse_mode="html",
-          disable_web_page_preview=True,
-          reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='⭕ Cʜᴀɴɴᴇʟ ⭕', url=f'https://t.me/TamilBots'),
-                                                 InlineKeyboardButton(text='⭕ Sᴜᴘᴘᴏʀᴛ ⭕', url=f'https://t.me/TamilBots') ],
-                                               [ InlineKeyboardButton(text='👨‍💻 Dᴇᴘʟᴏʏ Nᴏᴡ', url='https://youtube.com/c/TamilBots'),                                                
-                                                 InlineKeyboardButton(text='🔐 Cʟᴏꜱᴇ 🔐', url='https://youtube.com/c/TamilBots') ] ] ) )
-@Client.on_message(filters.command("about") & filters.private & filters.incoming)
-async def about(c, m, cb=False):
-    me = await c.get_me()
+
+RenamerNs.on_message(filters.command("help") & filters.private & filters.incoming)
+async def help(c, m, cb=False):
     button = [[
-        InlineKeyboardButton(text='🏡Home', callback_data='back'),
-        InlineKeyboardButton(text='🚪Close', callback_data="close")
+        InlineKeyboardButton(f'{HOUSE_WITH_GARDEN} Home', callback_data='back'),
+        InlineKeyboardButton(f'{MONEY_BAG} dev', url='https://t.me/tamilbots')
+        ],[
+        InlineKeyboardButton(f'{NO_ENTRY} 𝙲𝚕𝚘𝚜𝚎', callback_data='close')
     ]]
     reply_markup = InlineKeyboardMarkup(button)
     if cb:
         await m.message.edit(
-            text=TEXT.ABOUT.format(bot_name=me.mention),
+            text=script.HELP_USER.format(m.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=reply_markup
         )
     else:
         await m.reply_text(
-            text=TEXT.ABOUT.format(bot_name=me.mention),            
+            text=script.HELP_USER.format(m.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=reply_markup,
+            quote=True
+        )
+
+
+################## start commamd ##################
+
+@RenamerNs.on_message(filters.command("start") & filters.private & filters.incoming)
+async def start(c, m, cb=False):
+    owner_username = owner.username if owner.username else 'Ns_bot_updates'
+    button = [[
+        InlineKeyboardButton(f'{MAN_TEACHER_LIGHT_SKIN_TONE} My Owner', url=f'https://t.me/{owner_username}'),
+        InlineKeyboardButton(f'{ROBOT} About', callback_data='about')
+        ],[
+        InlineKeyboardButton(f'{INFORMATION} Help', callback_data="help"),
+        InlineKeyboardButton(f'{NO_ENTRY} Close', callback_data="close")
+    ]]
+    reply_markup = InlineKeyboardMarkup(button)
+    if cb:
+        await m.message.edit(
+            text=script.START_TEXT.format(bot_name=me.mention),
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+    else:
+        await m.reply_text(
+            text=script.START_TEXT.format(bot_name=me.mention),
+            disable_web_page_preview=True,
+            reply_markup=reply_markup,
+        ) 
+
+
+################## about command ##################
+
+@RenamerNs.on_message(filters.command("about") & filters.private & filters.incoming)
+async def about(c, m, cb=False):
+    me = await c.get_me()
+
+    button = [[
+        InlineKeyboardButton(f'{HOUSE_WITH_GARDEN} Home', callback_data='back'),
+        InlineKeyboardButton(f'{MONEY_BAG} dev', url='https://t.me/tamilbots')
+        ],[
+        InlineKeyboardButton(f'{NO_ENTRY} Close', callback_data="close")
+    ]]
+    reply_markup = InlineKeyboardMarkup(button)
+    if cb:
+        await m.message.edit(
+            text=script.ABOUT.format(bot_name=me.mention),
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+    else:
+        await m.reply_text(
+            text=script.ABOUT.format(bot_name=me.mention),
             disable_web_page_preview=True,
             reply_markup=reply_markup,
         )
+
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
