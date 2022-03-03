2ba9594aaaef48e83abdf6b14538df8f6698ea9d
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-04 00:12:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,3 +1,6 @@
+from pyrogram.emoji import *
+
+
 class script(object):
     START_TEXT = """ <b> 𝙷𝙴𝙻𝙾 {user_mention} ,\n 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 Rename Bot, I am file renamer bot, Please sent any telegram Document 📄 Or Video 📹 and enter new filenameto rename it. </b>  \n
 
@@ -5,3 +8,41 @@ class script(object):
 """
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+
+    DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
+    UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
+    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"
+    BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
+    NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
+    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+
+
+    ABOUT = """**𝖬𝗒 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 :**
+
+** My Name:** {bot_name}
+    
+** Language:** [Python 3](https://www.python.org/)
+
+** FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+** Developer:** {bot_owner}
+
+** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+
+** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+
+** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+"""
+
+    HELP_USER = """**Follow Below Steps:**
+   
+☞︎︎︎ Use /mode command to change upload mode.
+☞︎︎︎ Send a photo to set as permanent thumbnail.
+☞︎︎︎ Now send me the Telegram file you want to rename.
+☞︎︎︎ Send the new name when bot ask.
+
+For source code check about
+"""
