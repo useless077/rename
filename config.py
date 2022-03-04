7edb464f61b1ff0731ba273484bff4b9d4d1809e
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-04 11:43:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
  #   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
  #   BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
  #   CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
- #   FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
+    FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
  #   DATABASE_URL = os.environ.get("DATABASE_URL", "")
  #   DOWNLOAD_LOCATION = "./DOWNLOADS"
     try:
