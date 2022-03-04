2d68f9370111dfe604665afc6f42f3809c8f55dc
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-04 11:06:15 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -76,7 +76,18 @@ async def doc(bot,update):
      new_filename = name[1]
      file_path = f"downloads/{new_filename}"
      file = update.message.reply_to_message
+     trace_msg = None
      ms = await update.message.edit("``` Trying To Download...```")
+     if Config.TRACE_CHANNEL:
+         try:
+             media = await m.copy(chat_id=Config.TRACE_CHANNEL)
+             trace_msg = await media.reply_text(f'**User Name:** {m.from_user.mention(style="md")}\n\n**User Id:** `{m.from_user.id}`\n\n**New File Name:** `{new_file_name}`\n\n**Status:** Downloading....')
+         except PeerIdInvalid:
+             logger.warning("Give the correct Channel or Group ID.")
+         except ChannelInvalid:
+             logger.warning("Add the bot in the Trace Channel or Group as admin to send details of the users using your bot")
+         except Exception as e:
+             logger.warning(e)
      c_time = time.time()
      try:
      	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "``` Trying To Download...```",  ms, c_time   ))
