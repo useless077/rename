b9f617d84e7a794ac932c8bb86083c0e1c6db4c8
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-04 11:10:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ logger = logging.getLogger(__name__)
 class Config:
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID =  int(os.environ.get("OWNER_ID", 1169076058))
     AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS", "") else []
     if OWNER_ID not in AUTH_USERS:
         AUTH_USERS.append(OWNER_ID)
-    BANNED_USERS = [int(i) for i in os.environ.get("BANNED_USERS", "").split(" ")] if os.environ.get("BANNED_USERS", "") else None
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
-    CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
-    FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
+ #   BANNED_USERS = [int(i) for i in os.environ.get("BANNED_USERS", "").split(" ")] if os.environ.get("BANNED_USERS", "") else None
+ #   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ #   BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
+ #   CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
+ #   FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
+ #   DATABASE_URL = os.environ.get("DATABASE_URL", "")
+ #   DOWNLOAD_LOCATION = "./DOWNLOADS"
     try:
         TIME_GAP = int(os.environ.get("TIME_GAP", "")) if os.environ.get("TIME_GAP", "") else None
     except:
