c48c77c4027a6cd5b4c4c1d107a3842f3dfba006
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-19 01:09:16 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -100,9 +100,9 @@ async def doc(bot,update):
      old_file_name =f"downloads/{dow_file_name}"
      os.rename(old_file_name,file_path)
      user_id = int(update.message.chat.id)
-     thumb = find(user_id)
+     thumb_img = find(user_id)
      if thumb:
-     		ph_path = await bot.download_media(thumb)
+     		ph_path = await bot.download_media(thumb_img)
      		Image.open(ph_path).convert("RGB").save(ph_path)
      		img = Image.open(ph_path)
      		img.resize((320, 320))
